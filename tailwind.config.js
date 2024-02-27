664cc1aef167d4ec55fd37445880c9b6c4ed09ce
MarcioBastosN <marciobastosn@gmail.com>
2024-02-27 15:00:17 -0300

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{html,js}"],
+    presets: [
+        require('./vendor/wireui/wireui/tailwind.config.js')
+    ],
+    content: [
+        "./src/**/*.{html,js}",
+        './vendor/wireui/wireui/resources/**/*.blade.php',
+        './vendor/wireui/wireui/ts/**/*.ts',
+        './vendor/wireui/wireui/src/View/**/*.php'
+    ],
     theme: {
         extend: {},
     },
